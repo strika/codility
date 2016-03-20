6b0bbb4f0d21373c243294c524f7547493c98951
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2016-03-20 20:12:05 +0100

    Refactor Lesson 1

--- lib/codility/lesson_1.rb
+++ lib/codility/lesson_1.rb
@@ -5,15 +5,9 @@
 module Codility
   module Lesson1
     def self.solution(n)
-      matched_binary = n.to_s(2).match(/1.*1/)
-
-      if matched_binary
-        binary = matched_binary[0]
-        sequences_of_zeros = binary.split(/1+/)
-        sequences_of_zeros.map(&:size).max || 0
-      else
-        0
-      end
+      binary = n.to_s(2)[/1.*1/] || ""
+      sequences_of_zeros = binary.split(/1+/)
+      sequences_of_zeros.map(&:size).max || 0
     end
   end
 end
