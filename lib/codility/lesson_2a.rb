6c6b3f85f0f51e190b6af966e028c0a0f82c347a
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2016-03-21 07:16:48 +0100

    Solve Lesson2a

---
+++ lib/codility/lesson_2a.rb
@@ -0,0 +1,11 @@
+# CyclicRotation
+#
+# Rotate an array to the right by a given number of steps.
+#
+module Codility
+  module Lesson2a
+    def self.solution(a, k)
+      a.rotate(-k)
+    end
+  end
+end
