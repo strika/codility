9f973d3d02b21421ddbe0d8dbf01703272ddaf8a
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2016-03-23 09:03:34 +0100

    Solve Lesson 4c

---
+++ lib/codility/lesson_4c.rb
@@ -0,0 +1,16 @@
+# MissingInteger
+#
+# Find the minimal positive integer not occurring in a given sequence.
+#
+module Codility
+  module Lesson4c
+    def self.solution(a)
+      largest_possible_positive_integer = a.size + 1
+      positive_integers = SortedSet.new(1..largest_possible_positive_integer)
+
+      a.each { |n| positive_integers.delete(n) }
+
+      positive_integers.first
+    end
+  end
+end
