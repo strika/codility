80449dd7b532d80003e19bbcb255a562b497b646
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2016-03-24 11:49:41 +0100

    Solve Lesson 5c O(N)

--- lib/codility/lesson_5c.rb
+++ lib/codility/lesson_5c.rb
@@ -19,11 +19,35 @@ module Codility
       end
     end
 
-    def self.solution(s, p, q)
+    def self.solution_slow(s, p, q)
       impact_factors = decode(s)
 
       (0...p.size).map do |i|
         impact_factors[p[i]..q[i]].min
+      end
+    end
+
+    def self.solution(s, p, q)
+      impact_factors = decode(s)
+      minimum_impact_factors = Array.new(impact_factors.size)
+
+      (impact_factors.size - 1).downto(0).each do |i|
+        current_impact_factor = impact_factors[i]
+        leading_impact_factors = minimum_impact_factors[i + 1] || {}
+        minimum_impact_factors[i] = leading_impact_factors.merge(current_impact_factor => i)
+      end
+
+      (0...p.size).map do |i|
+        start_index = p[i]
+        end_index = q[i]
+        minimum_impact_factors_for_start_index = minimum_impact_factors[start_index]
+
+        minimum_impact_factor = (1..4).find do |factor|
+          f = minimum_impact_factors_for_start_index[factor]
+          f && f <= end_index
+        end
+
+        minimum_impact_factor || impact_factors[start_index]
       end
     end
   end
--- spec/codility/lesson_5c_spec.rb
+++ spec/codility/lesson_5c_spec.rb
@@ -4,6 +4,14 @@ require "benchmark"
 
 module Codility
   RSpec.describe Lesson5c do
+
+    def generate_p_and_q(m, n)
+      seq = (1..m).map { |_| [rand(n), rand(n)].sort }
+      p = seq.map { |a| a[0] }
+      q = seq.map { |a| a[1] }
+      [p, q]
+    end
+
     it "calculates impact factors" do
       s = "CAGCCTA"
       p = [2, 5, 0]
@@ -12,12 +20,40 @@ module Codility
       expect(Lesson5c.solution(s, p, q)).to eql([2, 4, 1])
     end
 
+    context "with short sequence" do
+      it "calculates impact factors" do
+        s = "AA"
+        p = [0]
+        q = [0]
+
+        expect(Lesson5c.solution(s, p, q)).to eql([1])
+      end
+    end
+
+    context "another short sequence" do
+      it "calculates impact factor" do
+        s = "AGA"
+        p = [0]
+        q = [1]
+
+        expect(Lesson5c.solution(s, p, q)).to eql(Lesson5c.solution_slow(s, p, q))
+      end
+    end
+
+    context "random test" do
+      it "calculates impact factors" do
+        letters = ["A", "C", "G", "T"]
+        s = (0..3).map { |_| letters[rand(3)].to_s }.join("")
+        p, q = generate_p_and_q(1, 2)
+
+        expect(Lesson5c.solution(s, p, q)).to eql(Lesson5c.solution_slow(s, p, q))
+      end
+    end
+
     context "with larga data set" do
       it "calculates impact factors" do
         s = "CAGTC" * 20000
-        seq = (1..50000).map { |_| [rand(100000), rand(100000)].sort }
-        p = seq.map { |a| a[0] }
-        q = seq.map { |a| a[1] }
+        p, q = generate_p_and_q(50000, 100000)
 
         time = Benchmark.realtime do
           expect(Lesson5c.solution(s, p, q)).to be_an_instance_of(Array)
