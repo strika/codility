52591ad86345362bb42b0885d0406133c7cb394c
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2016-03-28 11:07:15 +0200

    Solve Prefix Sums -> MinAvgTwoSlice

--- lib/codility/lesson_5d.rb
+++ lib/codility/lesson_5d.rb
@@ -2,30 +2,34 @@
 #
 # Find the minimal average of any slice containing at least two elements.
 #
+# The solution is based on the fact that the slice with the minimum average
+# must have 2 or 3 elements.
+#
+# Solution score - https://codility.com/demo/results/training7W29Y9-E8R/.
+#
 module Codility
   module Lesson5d
-    def self.average(slice)
-      slice.reduce(&:+).to_f / slice.size
-    end
-
     def self.solution(a)
-      slices = []
+      max_index = a.size - 1
+      min_average = (a[0] + a[1]) / 2.0
+      min_average_index = 0
 
-      (0..(a.size - 1)).each do |i|
-        ((i + 1)..a.size).each do |j|
-          slices << [i, j]
+      (0..max_index).each do |i|
+        sum = a[i]
+        j_max = [max_index, i + 2].min
+
+        ((i + 1)..j_max).each do |j|
+          sum += a[j]
+          average = sum.to_f / (j - i + 1)
+
+          if average < min_average
+            min_average = average
+            min_average_index = i
+          end
         end
       end
 
-      averages = slices.map do |i, j|
-        average(a[i, j])
-      end
-
-      min_average = averages.min
-
-      min_slice = slices.find { |i, j| average(a[i, j]) == min_average }
-
-      min_slice[0]
+      min_average_index
     end
   end
 end
