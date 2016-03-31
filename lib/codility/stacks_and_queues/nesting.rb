2d3812457440da3247c578e9629c250e494706e9
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2016-03-31 14:01:22 +0000

    Solve StacksAndQueues -> Nesting

---
+++ lib/codility/stacks_and_queues/nesting.rb
@@ -0,0 +1,14 @@
+# Nesting
+#
+# Determine whether given string of parentheses is properly nested.
+#
+module Codility
+  module StacksAndQueues
+    module Nesting
+      def self.solution(a)
+        # this task can be solved the same way as
+        # Codility::StacksAndQueues::Brackets
+      end
+    end
+  end
+end
---
+++ spec/codility/stacks_and_queues/nesting_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+require "codility/stacks_and_queues/nesting"
+
+module Codility
+  module StacksAndQueues
+    RSpec.describe Nesting do
+
+    end
+  end
+end
