9687138b7adc5a55c4822c8fe1ed864253e8a5d6
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2016-04-12 13:59:45 +0000

    Solve Fibonacci Numbers -> FibFrog (slow)

---
+++ lib/codility/fibonacci_numbers/fib_frog.rb
@@ -0,0 +1,46 @@
+# FibFrog
+#
+# Count the minimum number of jumps required for a frog to get to the other
+# side of a river.
+#
+module Codility
+  module FibonacciNumbers
+    module FibFrog
+      def self.solution(a)
+        n = a.size
+        fibs = fibonacci_numbers(n + 1).drop(1)
+        positions = [{:location => n, :steps => 0}]
+
+        while positions.any?
+          position = positions.shift
+
+          fibs.each do |f|
+            new_location = position[:location] - f
+
+            if new_location == -1
+              return position[:steps] + 1
+            elsif new_location >= 0 && a[new_location] == 1
+              positions << {:location => new_location, :steps => position[:steps] + 1}
+            end
+          end
+        end
+
+        -1
+      end
+
+      def self.fibonacci_numbers(limit)
+        a = 0
+        b = 1
+        fibs = [a, b]
+
+        while (n = a + b) <= limit
+          fibs << n
+          a = b
+          b = n
+        end
+
+        fibs
+      end
+    end
+  end
+end
---
+++ spec/codility/fibonacci_numbers/fib_frog_spec.rb
@@ -0,0 +1,54 @@
+require "spec_helper"
+require "codility/fibonacci_numbers/fib_frog"
+
+module Codility
+  module FibonacciNumbers
+    RSpec.describe FibFrog do
+      it "returns the minimum number of jumps" do
+        a = [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0]
+        expect(FibFrog.solution(a)).to eql(3)
+      end
+
+      it "returns the minimum number of jumps" do
+        a = [1, 0, 0, 0, 0]
+        expect(FibFrog.solution(a)).to eql(2)
+      end
+
+      it "returns the minimum number of jumps" do
+        expect(FibFrog.solution([1])).to eql(1)
+      end
+
+      context "for large data set" do
+        it "is fast" do
+          a = []
+
+          100_000.times { a << rand(1).round }
+
+          time = Benchmark.realtime do
+            expect(FibFrog.solution(a)).to be_an_instance_of(Fixnum)
+          end
+
+          expect(time).to be < 1
+        end
+
+        it "is fast" do
+          a = []
+
+          50_000.times { a.concat([0, 1]) }
+
+          time = Benchmark.realtime do
+            expect(FibFrog.solution(a)).to be_an_instance_of(Fixnum)
+          end
+
+          expect(time).to be < 1
+        end
+      end
+
+      describe ".fibonacci_numbers" do
+        it "returns fibonacci numbers" do
+          expect(FibFrog.fibonacci_numbers(100)).to eql([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
+        end
+      end
+    end
+  end
+end
